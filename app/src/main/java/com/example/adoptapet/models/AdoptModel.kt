66d57c0fd61a0d7ae59c10699a471ca4cb780f83
Maciej <maciejmarchel14@gmail.com>
2023-05-04 16:14:58 +0100

    Added google maps functionality and ability to add placemark on the map

--- app/src/main/java/com/example/adoptapet/models/AdoptModel.kt
+++ app/src/main/java/com/example/adoptapet/models/AdoptModel.kt
@@ -12,7 +12,13 @@ data class AdoptModel(var id: Long = 0,
                       var petAge: Int = 0,
                       var email: String = "",
                       var availableDate: String = "",
-                      var image: Uri = Uri.EMPTY) : Parcelable
+                      var image: Uri = Uri.EMPTY,
+                      var lat: Double = 0.0,
+                      var lng: Double = 0.0,
+                      var zoom: Float = 0f) : Parcelable
 
-
+@Parcelize
+data class Location(var lat: Double = 0.0,
+                    var lng: Double = 0.0,
+                    var zoom: Float = 0f) : Parcelable
 
